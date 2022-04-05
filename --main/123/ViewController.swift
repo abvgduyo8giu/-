4a321bceefa967bf30bb6228e6bf155fbbbbb66c
abvgduyo8giu <101309883+abvgduyo8giu@users.noreply.github.com>
2022-04-05 13:59:04 +0900

    Add files via upload

---
+++ --main/123/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  123
+//
+//  Created by Гость on 30.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var buttonZero: UIButton!
+    @IBOutlet weak var ButtonAC: UIButton!
+    @IBOutlet weak var staki: UIStackView!
+    @IBOutlet weak var lable: UILabel!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
